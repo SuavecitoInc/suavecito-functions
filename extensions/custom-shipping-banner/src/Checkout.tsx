721b301960a559d64f7f9fbf2f44b8f620719a46
j-Riv <jose.alfredo.rivera@gmail.com>
2024-06-06 13:14:27 -0700

    feature: custom checkout banner

---
+++ extensions/custom-shipping-banner/src/Checkout.tsx
@@ -0,0 +1,20 @@
+import { Banner, reactExtension } from "@shopify/ui-extensions-react/checkout";
+
+export default reactExtension(
+  "purchase.checkout.block.render",
+  ({ settings }) => <Extension settings={settings} />,
+);
+
+function Extension({ settings }) {
+  console.log("Extension settings", settings);
+
+  return (
+    <Banner
+      title={settings.current.title}
+      status={settings.current.status}
+      collapsible={settings.current.collapsible}
+    >
+      {settings.current.description}
+    </Banner>
+  );
+}
